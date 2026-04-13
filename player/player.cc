f3a1697bbae7f779b480fe2ad6b2f858b851a228
Orrim_SiGMono <szymala777@gmail.com>
2026-04-13 15:18:51 +0200

    to bugfix

--- player/player.cc
+++ player/player.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -16,24 +17,57 @@ class Player{
             this->nickname = nickname;
             this->money = 5000;                         //  Startowa ilość kasy do ustalenia
         }
-        Player(int id, string nickname, int money){     //  Konstruktor przy tworzeniu obiektu gracza z pliku players.txt
+        Player(int id, string nickname, int money){     //  Konstruktor przy tworzeniu obiektu currentPlayer za pomocą danych z pliku players.txt
             this->id = id;
             this->nickname = nickname;
             this->money = money;
         }
-        void setMoney(int money){                       //  Zapisywanie pieniędzy w formacie wstawiania nowej wartości
+        void setMoney(int money){                       //  Zapisywanie i pobieranie wartości
             this->money = money;
         }
-        int getMoney(){                                 //  Pobieranie pieniędzy
+        int getMoney(){                                 
             return this->money;
         }
-        string getNickname(){                           //  Pobieranie nazwy gracza (ustalanie nazwy gracza można zrobić tylko przy tworzeniu obiektu klasy)
+        string getNickname(){                           
             return this->nickname;
         }
 };
 
+Player getPlayer(string nickname){                      //  Pobieranie danych gracza z pliku players.txt
+    ifstream txtfile;
+    txtfile.open("players.txt");
+
+    string line;
+    if(txtfile.is_open()){                              //  Jak plik został otwarty to wyszukuje nazwy gracza w pliku players.txt i tworzy obiekt DYNAMICZNY aktualnego gracza currentPlayer
+        while(getline(txtfile, line)){
+            int res = line.find(nickname);
+            if(res != string::npos){
+                if(res-1 == ',' && res+nickname.length() == ','){
+                    auto pos = line.find(",");
+                    int id = stoi(line.substr(0,pos));
+                    line.erase(0,pos+1);
+                    pos = line.find(",");
+                    string nickname = line.substr(0,pos);
+                    line.erase(0,pos+1);
+                    int money = stoi((line));
+                    Player tempPlayer(id,nickname,money);
+                    return tempPlayer;
+                }
+            }
+        }
+    }else{                                              //  Jak jest błąd pliku to przekazuje błąd i wychodzi z funkcji
+        cerr<<"File failed to open or doesn't exist and failed to create current player"<<endl;
+    }
+    txtfile.close();
+}
+
+Player createPlayer(){
+    Player currentPlayer = getPlayer("TestPlayer");
+    cout<<currentPlayer.getNickname();
+}
+
 int main(int argc, char const *argv[])
 {
-    
+
     return 0;
 }

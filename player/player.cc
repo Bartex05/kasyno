d9fb9fabb617515d44eabc6094404d14afca19dc
Orrim_SiGMono <szymala777@gmail.com>
2026-04-13 13:00:45 +0200

    Added "Player" class, its constructors, setters and getters

---
+++ player/player.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+        //  Obiekty tej klasy będą wykorzystywane do zapisywania pieniędzy wygranych lub przegranych NA KOŃCU danej gry/stołu po czym będzie wpisywany do pliku players.txt
+class Player{
+    private:
+        int id;
+        int money;
+        string nickname;
+    public:
+        Player(int id, string nickname){                //  Konstruktor przy tworzeniu nowego obiektu gracza
+            this->id = id;
+            this->nickname = nickname;
+            this->money = 5000;                         //  Startowa ilość kasy do ustalenia
+        }
+        Player(int id, string nickname, int money){     //  Konstruktor przy tworzeniu obiektu gracza z pliku players.txt
+            this->id = id;
+            this->nickname = nickname;
+            this->money = money;
+        }
+        void setMoney(int money){
+            this->money = money;
+        }
+        int getMoney(){
+            return this->money;
+        }
+        string getNickname(){
+            return this->nickname;
+        }
+};
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}

05e0d00626b76637137b6619d690b31172381537
SiGMono <szymala777@gmail.com>
2026-04-26 12:42:54 +0200

    addedchanges

--- kasyno.cc
+++ kasyno.cc
@@ -48,7 +48,7 @@ class Player{
         int loadPlayer();
 };
 
-int Blackjack(int bank){
+int Blackjack(int bank){        //Schemat nie końcowa funkcja
     int bettingMoney;
     cin>>bettingMoney;
     bank = bank - bettingMoney;
@@ -64,5 +64,43 @@ void Ruletka(){
 
 int main()
 {
+    int gameChoice = -1;     //Ta zmienna jak i repeat jest umieszczona tutaj dla efektywnego powtarzania gry
+    int gameChoiceRepeat = -1;
+    Player player;
+    while(true){
+        cout<<"===>        WYBOR GRY        <==="<<endl;
     
+        cout<<"1. Blackjack"<<endl;         
+        cout<<"2. Ruletka"<<endl;           //Do dopisania więcej
+
+        cout<<"0. Wyjdz"<<endl;
+
+        cout<<"Prosze podac numer"<<endl;
+
+        while(true){                        //Podczas dopisywania większej ilości gier tutaj trzeba dodać jej numer w postaci warunku
+            if(gameChoice == 0){
+                return 0;
+            }
+            cin>>gameChoice;
+            if(gameChoice >= 1 && gameChoice <= 2/*Tutaj dopisywane sprawdzanie liczby gry w postaci gameChoice >= 1 && gameChoice <= *Aktualna liczba możliwych gier* */){         // <----------------------------
+                break;
+            }else if(gameChoice != 0){
+                cout<<"Nie ma gry o takim numerze"<<endl;
+            }
+        }
+
+        switch(gameChoice){
+            case 1:                         //Podczas dopisywania większej ilości gier tutaj trzeba dodać jej case w tym samym numerze co w menu tekstowym                          // <----------------------------
+                gameChoice = -1;
+                player.setMoney(Blackjack(player.getMoney()));
+                break;
+            case 2:
+                gameChoice = -1;
+                Ruletka();
+                break;
+            default:                        //Ze względu na zabezpieczony brak nieodpowiedniego case, default jest używany do powtarzania gry poprzez niezmianianą zmienną 'gameChoice'
+                cout<<"Brak wybranej gry"<<endl;
+                return 0;
+        }
+    }
 }
